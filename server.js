3ededa2af7a65afac1be6e8709ed002c36a1fb8e
Cory Nelson <fullnelsontech@gmail.com>
2022-08-02 20:10:55 -0400

    change procfile and port

--- server.js
+++ server.js
@@ -80,4 +80,6 @@ app.route("/remove/:id").get((req, res) => {
 })
 
 // Start Server
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
+app.listen(process.env.PORT || PORT, () =>
+  console.log(`Server is running on port ${PORT}`)
+)
